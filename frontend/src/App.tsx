bb6a43aaaf9567a69053ca9b17d2eda25642b00f
Enzo Bandinu <enzo.bandinu@gmail.com>
2026-08-06 12:19:51 +0200

    chore(frontend) : install React Router and declare application routes  [SCRUM-142]

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
+import DictaphonePage from './pages/DictaphonePage'
+import VideoModePage from './pages/VideoModePage'
 
 function App() {
-  return <LoginPage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dictaphone" element={<DictaphonePage />} />
+        <Route path="/video" element={<VideoModePage />} />
+        {/* URL inconnue : on renvoie à l'accueil plutôt que sur une page blanche */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
